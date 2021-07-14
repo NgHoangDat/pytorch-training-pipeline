f4bf31fdbf7e849b4af339cbce86d25f006e2d4d
datnh21 <v.datnh21@vinai.io>
2021-07-14 23:06:58 +0700

    update commands

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/commands/app.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/commands/app.py
@@ -1,6 +1,9 @@
+from functools import wraps
 from typing import *
 from typer import Typer
 
+
+__app = Typer()
 
 class Command:
 
@@ -13,18 +16,22 @@ class App:
     def __init__(self, *args, **kwargs) -> None:
         self.app = Typer(*args, **kwargs)
 
+    @wraps(__app.__call__)
     def __call__(self, *args, **kwargs) -> Any:
         return self.app(*args, **kwargs)
 
+    @wraps(__app.add_typer)
     def add_typer(self, *args, **kwargs):
         self.app.add_typer(*args, **kwargs)
 
+    @wraps(__app.callback)
     def callback(self, *args, **kwargs) -> Any:
         return self.app.callback(*args, **kwargs)
 
+    @wraps(__app.command)
     def command(self, *args, **kwargs):
         def decorator(func:Callable):
-            cls = type("Command", Command, {
+            cls = type("Command", (Command,), {
                 "__func": func
             })
             self.app.command(*args, **kwargs)(cls)
